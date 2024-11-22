6b381ac2423d351ada40a761db088b9ef6af1a51
hono1030 <honoka.n1030@gmail.com>
2024-11-22 17:21:58 +0900

    feat: implement i18n

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,23 @@
 // import SignUpForm from "../components/profile/SignUpForm";
 import SearchBar from "../components/search/SearchBar";
+import { useTranslation, Trans } from "react-i18next";
 
-const HomePage: React.FC = () => (
-  <div className="justify-items-center">
-    <div className="my-6">
-      <h1>Find a trusted Pet Sitter or list your services!</h1>
-    </div>
-    <h3>Search for a Pet Sitter</h3>
-    {/* <SignUpForm /> */}
-    <SearchBar />
-  </div>
-);
+const HomePage: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div className="justify-items-center">
+        <div className="my-6">
+          <h1>{t("title.subtitle")}</h1>
+        </div>
+        <h3>Search for a Pet Sitter</h3>
+        {/* <SignUpForm /> */}
+        <SearchBar />
+      </div>
+      ;
+    </>
+  );
+};
 
 export default HomePage;
