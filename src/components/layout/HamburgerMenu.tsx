3005c40828793884bccbd54e5c2b2e1581e7d451
hono1030 <honoka.n1030@gmail.com>
2024-11-24 18:57:25 +0900

    feat: style DashboardBookingsPage

--- src/components/layout/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu.tsx
@@ -15,26 +15,32 @@ const HamburgerMenu: React.FC<Props> = ({ menuRef, onClose, handleLogout }) => {
     <div
       ref={menuRef}
       //   onClick={(e) => e.stopPropagation()}
-      className="bg-white border border-transparent rounded w-32 absolute top-12 shadow-lg z-50"
+      className="bg-white border border-transparent rounded w-32 absolute top-12 shadow-custom z-50"
     >
       <ul className="list-none p-0 m-0">
-        <li className="p-4 border-b  border-slate-300">
+        <li>
           <Link to="/dashboard/account" onClick={onClose}>
-            {t("hamburger_menu.account")}
+            <div className="px-4 py-2 border-b  border-slate-300 hover:bg-zinc-100 text-sm">
+              {t("hamburger_menu.account")}
+            </div>
           </Link>
         </li>
-        <li className="p-4 border-b border-slate-300">
+        <li>
           <Link to="/dashboard/sitter_profile" onClick={onClose}>
-            {t("hamburger_menu.profile")}
+            <div className="px-4 py-2 border-b  border-slate-300 hover:bg-zinc-100 text-sm">
+              {t("hamburger_menu.profile")}
+            </div>
           </Link>
         </li>
-        <li className="p-4 border-b border-slate-300">
+        <li>
           <Link to="/dashboard/bookings" onClick={onClose}>
-            {t("hamburger_menu.bookings")}
+            <div className="px-4 py-2 border-b  border-slate-300 hover:bg-zinc-100 text-sm">
+              {t("hamburger_menu.bookings")}
+            </div>
           </Link>
         </li>
         <li
-          className="p-4 cursor-pointer"
+          className="px-4 py-2 border-slate-300 hover:bg-zinc-100 text-sm cursor-pointer"
           onClick={() => {
             onClose();
             handleLogout();
--- src/pages/DashboardBookingsPage.tsx
+++ src/pages/DashboardBookingsPage.tsx
@@ -1,29 +1,47 @@
 import React from "react";
+import { FaRegMessage } from "react-icons/fa6";
 
 const DashboardBookingsPage: React.FC = () => {
   return (
     <div>
-      <h2>Inquiries and Bookings</h2>
-      <div>
-        <h3>Pet Owner : Honoka</h3>
-        <p>2024/10/20 - 2024/10/22</p>
-        <p>Overnight Stay / Dog</p>
-        <p>Confirmed</p>
-        <p>Write Review</p>
-      </div>
-      <div>
-        <h3>Pet Owner : Honoka</h3>
-        <p>2024/10/20 - 2024/10/22</p>
-        <p>Overnight Stay / Dog</p>
-        <p>Confirmed</p>
-        <p>Write Review</p>
-      </div>
-      <div>
-        <h3>Pet Owner : Honoka</h3>
-        <p>2024/10/20 - 2024/10/22</p>
-        <p>Overnight Stay / Dog</p>
-        <p>Confirmed</p>
-        <p>Write Review</p>
+      <h2 className="mx-6 mb-2 font-bold text-2xl">Inquiries and Bookings</h2>
+      <div className="flex flex-col">
+        <div className="mx-6 my-2 border border-transparent shadow-custom rounded w-72 px-4 py-2 relative">
+          <h3 className="text-sm font-medium my-1">Booked with Honoka</h3>
+          <p className="text-xs text-gray-500 my-1">2024/10/20 - 2024/10/22</p>
+          <p className="text-xs text-gray-500 my-1">Overnight Stay / Dog</p>
+          <p className="text-xs text-gray-500 my-1">Confirmed</p>
+          <p className="text-xs underline my-1 cursor-pointer hover:text-lime-600">
+            Write Review
+          </p>
+          <div className="absolute top-4 right-4 hover:text-lime-600">
+            <FaRegMessage />
+          </div>
+        </div>
+        <div className="mx-6 my-2 border border-transparent shadow-custom rounded w-72 px-4 py-2 relative">
+          <h3 className="text-sm font-medium my-1">Booked with Honoka</h3>
+          <p className="text-xs text-gray-500 my-1">2024/10/20 - 2024/10/22</p>
+          <p className="text-xs text-gray-500 my-1">Overnight Stay / Dog</p>
+          <p className="text-xs text-gray-500 my-1">Confirmed</p>
+          <p className="text-xs underline my-1 cursor-pointer hover:text-lime-600">
+            Write Review
+          </p>
+          <div className="absolute top-4 right-4 hover:text-lime-600">
+            <FaRegMessage />
+          </div>
+        </div>
+        <div className="mx-6 my-2 border border-transparent shadow-custom rounded w-72 px-4 py-2 relative">
+          <h3 className="text-sm font-medium my-1">Booked with Honoka</h3>
+          <p className="text-xs text-gray-500 my-1">2024/10/20 - 2024/10/22</p>
+          <p className="text-xs text-gray-500 my-1">Overnight Stay / Dog</p>
+          <p className="text-xs text-gray-500 my-1">Confirmed</p>
+          <p className="text-xs underline my-1 cursor-pointer hover:text-lime-600">
+            Write Review
+          </p>
+          <div className="absolute top-4 right-4 hover:text-lime-600">
+            <FaRegMessage />
+          </div>
+        </div>
       </div>
     </div>
   );
