83bf84c0c66f08e3ee114ce5966e6999f643d1d8
hono1030 <honoka.n1030@gmail.com>
2024-11-26 13:39:40 +0900

    feat: style for desktop view

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => (
   <footer className="bg-green-500">
-    <nav className="flex items-center justify-between h-20 px-4 sm:px-6 lg:px-8">
+    <nav className="flex items-center justify-between h-12 px-4 sm:px-6 lg:px-8">
       {/* Logo */}
       <div className="text-white text-l">
         <Link to="/">
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -54,115 +54,123 @@ const Header: React.FC = () => {
 
   return (
     <header className="bg-green-500">
-      <nav className="flex items-center justify-between h-20 px-4 sm:px-6 lg:px-8">
+      <nav className="flex items-center justify-between h-12 px-4 sm:px-6 lg:px-8">
         {/* Logo */}
         <div className="text-white text-xl font-bold">
           <Link to="/">ぷぴぽ</Link>
         </div>
-        <div className="flex space-x-2">
-          {Object.keys(lngs).map((lng) => (
-            <button
-              className="text-white border border-white px-2 py-1 rounded hover:bg-white hover:text-green-500"
-              type="submit"
-              key={lng}
-              onClick={() => i18n.changeLanguage(lng)}
-              disabled={i18n.resolvedLanguage === lng}
-            >
-              {lngs[lng].nativeName}
-            </button>
-          ))}
-        </div>
-        {/* Desktop Navigation */}
-        <div className="hidden md:flex space-x-6 items-center">
-          {!currentUser ? (
-            <>
-              <Link to="/login" className="text-white">
-                {t("header.login")}
-              </Link>
-              <Link to="/signup" className="text-white">
-                {t("header.signup")}
-              </Link>
-            </>
-          ) : (
-            <>
-              <div className="relative">
-                <span
-                  ref={triggerRef}
-                  onClick={() => setMenuOpen((prev) => !prev)}
-                  className="cursor-pointer text-white"
+        <div className="flex space-x-8">
+          <div className="flex space-x-1">
+            {Object.keys(lngs).map((lng) => (
+              <div
+                className="inline-flex"
+                role="group"
+                aria-label="Button group"
+              >
+                <button
+                  className="text-white border border-white px-1 py-1 rounded hover:bg-white hover:text-green-500 transition-colors duration-150 text-xs"
+                  type="submit"
+                  key={lng}
+                  onClick={() => i18n.changeLanguage(lng)}
+                  disabled={i18n.resolvedLanguage === lng}
                 >
-                  {currentUser.email}
-                </span>
-                {menuOpen && (
-                  <div
-                    ref={menuRef}
-                    className="absolute right-0 mt-2 w-48 bg-white rounded shadow-lg z-50"
-                  >
-                    <Link
-                      to="/dashboard/account"
-                      className="block px-4 py-2 hover:bg-gray-200"
-                      onClick={toggleMenu}
-                    >
-                      {t("hamburger_menu.account")}
-                    </Link>
-                    <Link
-                      to="/dashboard/bookings"
-                      className="block px-4 py-2 hover:bg-gray-200"
-                      onClick={toggleMenu}
-                    >
-                      {t("hamburger_menu.bookings")}
-                    </Link>
-                    <Link
-                      to="/dashboard/sitter_profile"
-                      className="block px-4 py-2 hover:bg-gray-200"
-                      onClick={toggleMenu}
-                    >
-                      {t("hamburger_menu.become_sitter")}
-                    </Link>
-                    <Link
-                      to="/dashboard/requests"
-                      className="block px-4 py-2 hover:bg-gray-200"
-                      onClick={toggleMenu}
-                    >
-                      {t("hamburger_menu.requests")}
-                    </Link>
-                    <button
-                      onClick={() => {
-                        handleLogout();
-                        toggleMenu();
-                      }}
-                      className="block w-full text-left px-4 py-2 hover:bg-gray-200"
-                    >
-                      {t("hamburger_menu.logout")}
-                    </button>
-                  </div>
-                )}
+                  {lngs[lng].nativeName}
+                </button>
               </div>
-            </>
-          )}
-        </div>
+            ))}
+          </div>
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex space-x-6 items-center">
+            {!currentUser ? (
+              <>
+                <Link to="/login" className="text-white">
+                  {t("header.login")}
+                </Link>
+                <Link to="/signup" className="text-white">
+                  {t("header.signup")}
+                </Link>
+              </>
+            ) : (
+              <>
+                <div className="relative">
+                  <span
+                    ref={triggerRef}
+                    onClick={() => setMenuOpen((prev) => !prev)}
+                    className="cursor-pointer text-white"
+                  >
+                    {currentUser.email}
+                  </span>
+                  {menuOpen && (
+                    <div
+                      ref={menuRef}
+                      className="absolute right-0 mt-2 w-48 bg-white rounded shadow-lg z-50"
+                    >
+                      <Link
+                        to="/dashboard/account"
+                        className="block px-4 py-2 hover:bg-gray-200"
+                        onClick={toggleMenu}
+                      >
+                        {t("hamburger_menu.account")}
+                      </Link>
+                      <Link
+                        to="/dashboard/bookings"
+                        className="block px-4 py-2 hover:bg-gray-200"
+                        onClick={toggleMenu}
+                      >
+                        {t("hamburger_menu.bookings")}
+                      </Link>
+                      <Link
+                        to="/dashboard/sitter_profile"
+                        className="block px-4 py-2 hover:bg-gray-200"
+                        onClick={toggleMenu}
+                      >
+                        {t("hamburger_menu.become_sitter")}
+                      </Link>
+                      <Link
+                        to="/dashboard/requests"
+                        className="block px-4 py-2 hover:bg-gray-200"
+                        onClick={toggleMenu}
+                      >
+                        {t("hamburger_menu.requests")}
+                      </Link>
+                      <button
+                        onClick={() => {
+                          handleLogout();
+                          toggleMenu();
+                        }}
+                        className="block w-full text-left px-4 py-2 hover:bg-gray-200"
+                      >
+                        {t("hamburger_menu.logout")}
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
 
-        {/* Mobile Hamburger Icon */}
-        <div className="md:hidden flex items-center">
-          <button
-            onClick={toggleMobileMenu}
-            className="text-white focus:outline-none"
-          >
-            <svg
-              className="w-6 h-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+          {/* Mobile Hamburger Icon */}
+          <div className="md:hidden flex items-center">
+            <button
+              onClick={toggleMobileMenu}
+              className="text-white focus:outline-none"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
+              <svg
+                className="w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </nav>
 
