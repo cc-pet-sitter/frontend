5f76769f6af3f685924bc87a7e6f95d0a8f3ced8
hono1030 <honoka.n1030@gmail.com>
2024-12-08 16:20:09 +0900

    Chore:style pet profile page for desktop view

--- src/pages/DashboardPetsProfilePage.tsx
+++ src/pages/DashboardPetsProfilePage.tsx
@@ -46,7 +46,7 @@ const DashboardPetsProfilePage: React.FC = () => {
   }
 
   return (
-    <div className="dashboard-container">
+    <div className="dashboard-container md:m-14">
       {showEditProfileForm ? (
         // When clicking in "Edit profile"
         <div className="">
@@ -81,74 +81,76 @@ const DashboardPetsProfilePage: React.FC = () => {
                 {t("dashboard_pets_profile_page.title")}
               </h1>
               <div className="flex flex-col">
-                {petProfiles?.map((profile, index) => (
-                  <div
-                    key={index}
-                    className="mx-6 my-3 border border-transparent shadow-custom rounded w-80 sm:w-100 p-4"
-                  >
-                    <div className="sm:mt-0 sm:ml-6 flex items-center justify-between gap-4">
-                      <div className="relative h-20 w-20">
-                        {/* Loader */}
-                        {!imageLoaded && (
-                          <div className="absolute inset-0 flex items-center justify-center bg-gray-200 rounded-full">
-                            <TailSpin
-                              height="50"
-                              width="50"
-                              color="#fabe25"
-                              ariaLabel="loading"
+                <div className="md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-2 md:gap-y-4 md:justify-stretch">
+                  {petProfiles?.map((profile, index) => (
+                    <div
+                      key={index}
+                      className="mx-6 my-3 border border-transparent shadow-custom rounded w-80 sm:w-100 p-4"
+                    >
+                      <div className="sm:mt-0 sm:ml-6 flex items-center justify-between gap-4">
+                        <div className="relative h-20 w-20">
+                          {/* Loader */}
+                          {!imageLoaded && (
+                            <div className="absolute inset-0 flex items-center justify-center bg-gray-200 rounded-full">
+                              <TailSpin
+                                height="50"
+                                width="50"
+                                color="#fabe25"
+                                ariaLabel="loading"
+                              />
+                            </div>
+                          )}
+                          {/* Profile Picture */}
+                          {profile.profile_picture_src ? (
+                            <img
+                              src={profile.profile_picture_src}
+                              alt={`Picture of ${profile.name}`}
+                              className={`h-20 w-20 rounded-full object-cover ${
+                                imageLoaded ? "block" : "hidden"
+                              }`}
+                              onLoad={() => setImageLoaded(true)}
+                              onError={() => setImageLoaded(true)}
                             />
-                          </div>
-                        )}
-                        {/* Profile Picture */}
-                        {profile.profile_picture_src ? (
-                          <img
-                            src={profile.profile_picture_src}
-                            alt={`Picture of ${profile.name}`}
-                            className={`h-20 w-20 rounded-full object-cover ${
-                              imageLoaded ? "block" : "hidden"
-                            }`}
-                            onLoad={() => setImageLoaded(true)}
-                            onError={() => setImageLoaded(true)}
-                          />
-                        ) : (
-                          <PiDog className="h-20 w-20 text-gray-400" />
-                        )}
-                      </div>
+                          ) : (
+                            <PiDog className="h-20 w-20 text-gray-400" />
+                          )}
+                        </div>
 
-                      <div>
-                        <h2 className="font-medium my-1 text-lg">
-                          {profile.name}
-                        </h2>
                         <div>
-                          {/* Edit Profile */}
-                          <button
-                            onClick={() => {
-                              setSelectedPetProfile(profile);
-                              setShowEditProfileForm(true);
-                            }}
-                            className="text-brown text-sm underline mr-4 "
-                          >
-                            <a>
-                              {t("dashboard_pets_profile_page.edit_button")}
-                            </a>
-                          </button>
-                          {/* View Profile */}
-                          <button
-                            onClick={() => {
-                              setSelectedPetProfile(profile);
-                              setShowProfileView(true);
-                            }}
-                            className="text-brown text-sm underline mr-4"
-                          >
-                            <a>
-                              {t("dashboard_pets_profile_page.view_button")}
-                            </a>
-                          </button>
+                          <h2 className="font-medium my-1 text-lg">
+                            {profile.name}
+                          </h2>
+                          <div>
+                            {/* Edit Profile */}
+                            <button
+                              onClick={() => {
+                                setSelectedPetProfile(profile);
+                                setShowEditProfileForm(true);
+                              }}
+                              className="text-brown text-sm underline mr-4 "
+                            >
+                              <a>
+                                {t("dashboard_pets_profile_page.edit_button")}
+                              </a>
+                            </button>
+                            {/* View Profile */}
+                            <button
+                              onClick={() => {
+                                setSelectedPetProfile(profile);
+                                setShowProfileView(true);
+                              }}
+                              className="text-brown text-sm underline mr-4"
+                            >
+                              <a>
+                                {t("dashboard_pets_profile_page.view_button")}
+                              </a>
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
                 <button
                   onClick={() => setShowEditProfileForm(true)}
                   className="mx-6 my-4 btn-secondary py-1 px-2 rounded w-40 text-sm"
