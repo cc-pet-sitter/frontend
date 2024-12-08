5b592ef00930a87ea27183265895e941bf9b4be3
Michael Whipps <michaelwhipps@MacBook-Pro.local>
2024-12-08 21:27:10 +0900

    chore: style write review, add error message to jsx and add japanese

--- src/components/reviews/WriteReview.tsx
+++ src/components/reviews/WriteReview.tsx
@@ -24,6 +24,8 @@ const WriteReview: React.FC<WriteReviewProps> = ({
 }) => {
   const { register, handleSubmit } = useForm<ReviewForm>();
   const [ratingValue, setRatingValue] = useState<number | null>(null);
+  const [ratingError, setRatingError] = useState<boolean>(false);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
 
   const { currentUser } = useAuth();
 
@@ -32,11 +34,17 @@ const WriteReview: React.FC<WriteReviewProps> = ({
   console.log(booking);
 
   const inputClass =
-    "appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white";
+    "appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 text-lg　mb-2 leading-tight focus:outline-none focus:bg-white";
   const labelClass =
-    "block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2";
+    "block tracking-wide text-gray-700 font-bold mb-2 mt-2 text-lg";
 
   const onReviewSubmit = async (data: ReviewForm) => {
+    if (ratingValue === null) {
+      setRatingError(true);
+      return;
+    }
+
+    setRatingError(false);
     const isReviewOfSitter = recipientType === "sitter";
     const authorID = isReviewOfSitter
       ? booking.owner_appuser_id
@@ -70,8 +78,8 @@ const WriteReview: React.FC<WriteReviewProps> = ({
         throw new Error("Failed to submit review");
       }
 
-      alert("Review submitted.");
-      onClose();
+      // alert("Review submitted succesfully.");
+      setShowPopup(true);
     } catch (error) {
       console.error("Error submitting review:", error);
       alert("Cannot leave zero stars");
@@ -79,24 +87,31 @@ const WriteReview: React.FC<WriteReviewProps> = ({
   };
 
   return (
-    <div className="flex justify-center p-8">
+    <div className="flex justify-center p-6">
       <form
         onSubmit={handleSubmit(onReviewSubmit)}
         className="w-full max-w-lg "
+        noValidate
       >
-        <div className="md:flex md:items-center">
+        <div className="flex flex-col items-center md:items-start">
           <label className={`${labelClass} block`}>
             {t("review_input_page.rating")}
           </label>
-          <div className="md:w-1/2 flex justify-center py-6">
+          <div className="w-full flex justify-center p-2">
             <Rating
               name="simple-controlled"
               value={ratingValue}
               onChange={(_event, ratingValue) => {
                 setRatingValue(ratingValue);
               }}
+              size={"large"}
             />
           </div>
+          {ratingError && (
+            <p className="text-red-500 text-xs italic">
+              {t("review_input_page.rating_required")}
+            </p>
+          )}
         </div>
         <div className="mb-6">
           <label className={`${labelClass} block`} htmlFor="comment">
@@ -111,7 +126,7 @@ const WriteReview: React.FC<WriteReviewProps> = ({
           />
         </div>
 
-        <div className="md:flex md:items-center">
+        <div className="flex justify-center">
           <div className="md:w-1/2 flex justify-center">
             <button type="submit" className="btn-primary">
               {t("review_input_page.submit")}
@@ -119,6 +134,24 @@ const WriteReview: React.FC<WriteReviewProps> = ({
           </div>
         </div>
       </form>
+      {showPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white p-6 rounded shadow-lg text-center">
+            <p className="text-lg font-semibold">
+              {t("review_input_page.review_submitted")}
+            </p>
+            <button
+              className="btn-primary mt-4"
+              onClick={() => {
+                setShowPopup(false);
+                onClose();
+              }}
+            >
+              {t("review_input_page.close")}
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
